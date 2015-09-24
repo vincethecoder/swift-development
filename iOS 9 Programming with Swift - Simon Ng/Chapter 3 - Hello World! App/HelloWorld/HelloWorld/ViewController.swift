7649304f73946cba058ac9b2de5ef787921509ce
Kobe Sam <Vincent.Sam@capitalone.com>
2015-09-24 08:50:56 -0400

    Adding Chapters 2 & 3

---
+++ iOS 9 Programming with Swift - Simon Ng/Chapter 3 - Hello World! App/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Kobe Sam on 9/23/15.
+//  Copyright © 2015 KobeScript. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+
+
+}
+
