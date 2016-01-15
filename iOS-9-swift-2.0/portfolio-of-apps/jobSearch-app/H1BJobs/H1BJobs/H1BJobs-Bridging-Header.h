812ed5afba4d9bffb04b8f1d2e88fc7213904bc0
Kobe Sam <Vincent.Sam@capitalone.com>
2016-01-14 22:44:29 -0500

    Added a Bridging Header to accommodate objective C files.

---
+++ iOS-9-swift-2.0/portfolio-of-apps/jobSearch-app/H1BJobs/H1BJobs/H1BJobs-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  H1BJobs-Bridging-Header.h
+//  H1BJobs
+//
+//  Created by Kobe Sam on 1/14/16.
+//  Copyright © 2016 vincethecoder. All rights reserved.
+//
+
+#ifndef H1BJobs_Bridging_Header_h
+#define H1BJobs_Bridging_Header_h
+
+
+#endif /* H1BJobs_Bridging_Header_h */
