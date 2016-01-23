84df85c236a4a3f383764e42e05fd47b5d7a8b61
Kobe Sam <Vincent.Sam@capitalone.com>
2016-01-23 15:01:48 -0500

    Modified Spinner color and added cloud assets.

--- iOS-9-swift-2.0/portfolio-of-apps/jobSearch-app/H1BJobs/H1BJobs/GMDCircleLoader.h
+++ iOS-9-swift-2.0/portfolio-of-apps/jobSearch-app/H1BJobs/H1BJobs/GMDCircleLoader.h
@@ -30,7 +30,7 @@
 //----------------------------------
 // To change the color and frame size of the spinner, simply change the color and frame definition here.
 //----------------------------------
-#define GMD_SPINNER_COLOR       [UIColor colorWithRed:0.129 green:0.455 blue:0.627 alpha:1.0]
+#define GMD_SPINNER_COLOR       [UIColor colorWithRed:1.0/255.0 green:81/255.0 blue:125/255.0 alpha:1.0]
 #define GMD_SPINNER_FRAME       CGRectMake(40.0f, 40.0f, 40.0f, 40.0f)
 #define GMD_SPINNER_IMAGE       CGRectMake(15, 15,30,30)
 #define GMD_IMAGE               [UIImage imageNamed:@"image"]
