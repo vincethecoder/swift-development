738182cd5310f7084a901a215a6d3b968926149e
Kobe Sam <Vincent.Sam@capitalone.com>
2016-01-23 21:36:55 -0500

    Removed SQLiteCipher and cleaned up pods.

--- iOS-9-swift-2.0/portfolio-of-apps/jobSearch-app/H1BJobs/H1BJobs/AppDelegate.swift
+++ iOS-9-swift-2.0/portfolio-of-apps/jobSearch-app/H1BJobs/H1BJobs/AppDelegate.swift
@@ -39,7 +39,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Optional: configure GAI options.
         let gai = GAI.sharedInstance()
         gai.trackUncaughtExceptions = true  // report uncaught exceptions
-        gai.logger.logLevel = GAILogLevel.Verbose  // remove before app release
         
         return true
     }
--- iOS-9-swift-2.0/portfolio-of-apps/jobSearch-app/H1BJobs/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ iOS-9-swift-2.0/portfolio-of-apps/jobSearch-app/H1BJobs/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -84,12 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods/SQLCipher.framework"
   install_framework "Pods/SQLite.framework"
-  install_framework "Pods/SQLiteCipher.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods/SQLCipher.framework"
   install_framework "Pods/SQLite.framework"
-  install_framework "Pods/SQLiteCipher.framework"
 fi
